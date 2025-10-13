5b13bbb743f9295abee61f4081c52f9664d92f85
Ubaid Ullah <itzobii1@gmail.com>
2025-10-14 00:51:45 +0500

    app routes added

--- lib/core/routes/app_routes.dart
+++ lib/core/routes/app_routes.dart
@@ -1,55 +1,33 @@
+
+
+import 'package:fit_mind/core/routes/routes_names.dart';
+import 'package:fit_mind/view/home/home_screen.dart';
 import 'package:fit_mind/view/on_boarding/goal_screen.dart';
+import 'package:fit_mind/view/splash/splash_screen.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-// import '../../view/onboarding/permission_screen.dart';
-// import '../../view/auth/login_screen.dart';
-// import '../../view/auth/signup_screen.dart';
-// import '../../view/stats/stats_screen.dart';
-// import '../../view/ai_chat/chat_screen.dart';
-// import '../../view/profile/profile_screen.dart';
 
-class AppRoutes {
-  static const splash = '/';
-  static const onboarding = '/onboarding';
-  static const home = '/home';
-  static const stats = '/stats';
-  static const chat = '/chat';
-  static const profile = '/profile';
+class Routes {
 
-  static Map<String, WidgetBuilder> routes = {
-    splash: (context) => const SplashScreen(),
-    onboarding: (context) => const GoalScreen(),
-    // home: (context) => const HomeScreen(),
-    // stats: (context) => const StatsScreen(),
-    // chat: (context) => const ChatScreen(),
-    // profile: (context) => const ProfileScreen(),
-  };
-}
+  static Route<dynamic> generateRout(RouteSettings settings) {
+    switch(settings.name) {
+      case RoutesNames.splash:
+        return MaterialPageRoute(builder: (BuildContext context) => SplashScreen());
 
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
+      case RoutesNames.onBoarding:
+        return MaterialPageRoute(builder: (BuildContext context) => GoalScreen());
 
-  @override
-  State<SplashScreen> createState() => _SplashScreenState();
-}
+      case RoutesNames.home:
+        return MaterialPageRoute(builder: (BuildContext context) => HomeScreen());
 
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, AppRoutes.onboarding);
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          "Hal – AI Health Coach",
-          style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-        ),
-      ),
-    );
+      default :
+        return MaterialPageRoute(builder: (_) {
+          return const Scaffold(
+            body: Center(
+              child: Text("No route Found"),
+            ),
+          );
+        });
+    }
   }
 }
