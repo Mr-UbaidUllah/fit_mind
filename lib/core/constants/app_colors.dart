97c4cd338b56f0ab77515bc6f89948d22e43117f
Ubaid Ullah <itzobii1@gmail.com>
2025-10-16 16:13:32 +0500

    app color added

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,8 +1,12 @@
-
-
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xFF2F80ED); // blue
+  // Light mode
+  static const Color primary = Color(0xFF2F80ED);
+  static const Color accent = Color(0xFF56CCF2);
   static const Color background = Color(0xFFF9FAFB);
+
+  // Dark mode
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkSurface = Color(0xFF1E1E1E);
 }
